3f8a1a8a09c382826533d118c74a4b44a1872107
Drinqaka <qakadrin@gmail.com>
2026-03-05 17:42:17 +0100

    moduli26

--- moduli 26/main.js
+++ moduli 26/main.js
@@ -1,1 +1,67 @@
-console.log("Helloooo")
+console.log("Helloooo");
+
+var x = 10;
+var value = "Digitalschool";
+var y = 30;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+var a = 99;
+
+a++;
+
+console.log(a);
+
+var b = 101;
+
+b--;
+
+console.log(b);
+
+var c = 500;
+
+c+=500;
+
+console.log(c);
+
+var d = 600;
+
+d-=500;
+
+console.log(d);
+
+var e = 300;
+
+e*=4;
+
+console.log(e);
+
+var f = 1200;
+
+f/=4;
+
+console.log(f);
+
+var v = 100;
+
+var w = 50;
+
+console.log(v==w);
+console.log(v!=w);
+console.log(v>w);
+console.log(v<w);
+
+var emri = "Drin"
+
+var mbiemri = "Qaka"
+
+var fullName = emri + " " + mbiemri;
+
+console.log(fullName);
+
+console.log(`hello from ${emri}`);
+
+console.log('hello from' + ' ' + emri);
