bc9cc46130d9e6a339052fee76eed6164a221f72
Drinqaka <qakadrin@gmail.com>
2026-03-26 17:48:46 +0100

    moduli 30

---
+++ moduli30/main.js
@@ -0,0 +1,26 @@
+// var mosha = 7
+
+// if(mosha<8){
+//     document.write('Your child is to young to start programing');
+// }
+// else{
+//     document.write('We will find the groups');
+// }
+
+var age =  20
+
+if(age<8){
+    document.write('Your child is to young to start programing');
+}
+else if(age>18){
+    document.write('Your child is to old brooo');
+}
+else{
+    document.write('We will find the groups');
+}
+
+var ages = 20
+
+if(8>ages<18){
+    document.write('We will find the groups');
+}
