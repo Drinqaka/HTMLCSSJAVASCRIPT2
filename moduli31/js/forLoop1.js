f635f23366302fc40a481931173c965a23135be2
Drinqaka <qakadrin@gmail.com>
2026-04-21 17:44:38 +0200

    moduli31

--- moduli31/js/forLoop1.js
+++ moduli31/js/forLoop1.js
@@ -16,8 +16,26 @@ for(i=0; i < cars.length; i++) {
     text += cars[i] + '<br>';
 }
 
+document.getElementById('forLoop1').innerHTML = text;
+console.log(text);
+
 var text1 = '';
 
 for(x = 0; x < 5; x++) {
     text1 += "The number is" + x + "<br>"
 }
+
+document.getElementById('forLoop1').innerHTML = text;
+console.log(text);
+
+var person = {firstName: "John", lastName: "Doc" , age: 25};
+
+var text = "";
+var x;
+
+for(x in person){
+    text += person[x] + "<br>";
+}
+
+document.getElementById("forinloop").innerHTML = text;
+console.log(text);
